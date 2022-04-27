51b4a0c67b1b2a618136b172f93754c2a220e477
Hafizah Hassan <hafizahfereena@gmail.com>
2022-04-27 12:14:39 +0800

    menambahakan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello pizzah</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere ducimus nihil modi porro architecto quae laborum doloremque facilis rem! Aspernatur fuga expedita enim a labore neque praesentium nobis modi iusto.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
